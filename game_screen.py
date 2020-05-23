42ffa99b8a85ed631d0c2b36e370ca2fcd20af71
kaskang99 <lucask1@al.insper.edu.br>
2020-05-23 14:27:41 -0300

    tela que o jogador vai ver

---
+++ game_screen.py
@@ -0,0 +1,11 @@
+import pygame
+from config import 
+from assets importfrom sprites import
+from sprites import
+
+#---tela do jogo---
+def game_screen(window):
+    clock = pygame.time.Clock()
+
+    assets = load_assets()
+
