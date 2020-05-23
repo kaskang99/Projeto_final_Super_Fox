1c2fbdc34d222bee530658d70fcbefb5bb4d9efe
kaskang99 <lucask1@al.insper.edu.br>
2020-05-23 14:28:03 -0300

    configuraçãoes gerais, variáveis globais do jogo

---
+++ config.py
@@ -0,0 +1,29 @@
+# game config
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+TITLE = 'SUPER FOX'
+
+# player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAVITY = 1
+
+#iniciar plataformas
+PLATFORM_LIST = [(0, HEIGHT -40, WIDTH, 40), 
+    (WIDTH/2 - 50, HEIGHT * 3/4, 100, 20), 
+    (125, HEIGHT-350, 100, 20),
+    (WIDTH/3, HEIGHT*1/2, 50, 20)]
+
+# colors
+WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+TEAL = (100, 240, 255)
+LIGHTGREY = (100, 100, 100)
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH/TILESIZE
+GRIDHEIGHT = HEIGHT/32
