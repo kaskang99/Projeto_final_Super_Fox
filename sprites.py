26e2a108b8ef2be4aa0d83ed477f05cf97d86507
kaskang99 <lucask1@al.insper.edu.br>
2020-06-01 18:04:42 -0300

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -7,8 +7,11 @@ class Player(pg.sprite.Sprite):
     def __init__(self, game): #no arquivo MAIN.py - self.player = Player(self) - o init precisa de mais de 1 argumento, já que não é mais Player() e sim Player(self)
         pg.sprite.Sprite.__init__(self)
         self.game = game #Agora a classe Player tem como referência o jogo.
+        #self.walking = False
+        #self.jumping = False
+        #self.current_frame = 0
+        #self.last_update = 0
         self.image = self.game.fox_sprite.get_image(56, 4, 46, 35).convert()
-        #self.image.set_colorkey(SPRITE_BG)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/2, 600)
         self.pos = vec(WIDTH/5, HEIGHT/2)
